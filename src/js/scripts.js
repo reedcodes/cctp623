731f6e3d85d8bcd5d25dd0874bdb7d7a61e694ab
Reed Piernock <reed.piernock@georgetown.edu>
2020-12-13 18:04:25 -0500

    grab info from json

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,17 @@
 $(document).ready( () => {
 
-  const attributes = 'data/attributes.json';
+  const attributes = '../../data/attributes.json';
+  const remix = $( 'button.remix' );
 
-  $.getJSON( attributes, (data) => {
-  } );
+  remix.on( 'click', ( e ) => {
+    e.preventDefault();
 
-} );
+    $.getJSON( attributes, function( data ) {
+      $.each( data, function( i, person ) {
+        $( '#remixed' ).append( person.name + ' ' );
+      });
+    });
+
+  });
+
+});
