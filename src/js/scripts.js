0556975b00d55eade61b4552b890e2d4422cc0fb
Reed Piernock <reed.piernock@georgetown.edu>
2020-12-14 00:35:02 -0500

    check for required values

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -9,35 +9,40 @@ $(document).ready( () => {
 
   // When the "remix" button is clicked or entered...
   remix.on( 'click', ( e ) => {
-    // Stop the default behavior so we can run the remixes.
-    e.preventDefault();
 
     // Grab the user-selected attributes from the form.
     let gender = $( 'fieldset.gender input:checked' ).val(),
         ethnicity = $( 'fieldset.ethnicity input:checked' ).val();
 
-    // Declare other variables we need.
-    let name = '',
-        photo = '';
+    // Only remix the story if there are selected values...
+    if( gender != null && ethnicity != null ) {
 
-    // Grab the defined attributes from the JSON.
-    $.getJSON( people, ( data ) => {
+      // Stop the default behavior so we can run the remixes.
+      e.preventDefault();
 
-      // Loop through each person.
-      $.each( data, ( i, person ) => {
-        // Find the person who matches the user-selected attributes.
-        if( gender === person.gender && ethnicity === person.ethnicity ) {
-          name = person.name;
-          photo = 'photos/' + person.photo + '.jpg';
-        }
+      // Declare some variables we need.
+      let name = '',
+          photo = '';
+
+      // Grab the defined attributes from the JSON.
+      $.getJSON( people, ( data ) => {
+
+        // Loop through each person.
+        $.each( data, ( i, person ) => {
+          // Find the person who matches the user-selected attributes.
+          if( gender === person.gender && ethnicity === person.ethnicity ) {
+            name = person.name;
+            photo = 'photos/' + person.photo + '.jpg';
+          }
+        });
+
+        // Display the remixed text.
+        $( '#story' ).addClass( 'remixed' );
+        $( '#story .name' ).html( name );
+        $( '#story .photo' ).attr( 'src', photo );
+
       });
-
-      // Display the remixed text.
-      $( '#story' ).addClass( 'remixed' );
-      $( '#story .name' ).html( name );
-      $( '#story .photo' ).attr( 'src', photo );
-
-    });
+    }
 
   });
 
